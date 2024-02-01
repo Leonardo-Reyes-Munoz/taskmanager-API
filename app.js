1d0b9228bfb5e2de0394c68ca31279e8e92cea95
Leonardo Reyes Munoz <112450932+Leonardo-Reyes-Munoz@users.noreply.github.com>
2024-01-31 18:13:41 -0800

    feat(app.js): create basic routes for sessions and tasks.

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const app = express();
 
+// Landing Page
 app.get('/', (req, res) => {
-  res.send('Hello from testing route');
+  res.send('Landing Page');
 });
 
+// Registration routes
+app.get('/api/v1/sessions', (req, res) => {
+  res.send('Hello from Users Route');
+});
+
+//Tasks routes
+app.get('/api/v1/tasks', (req, res) => {
+  res.send('These are your tasks routes');
+});
 const port = process.env.PORT || 3000;
 
 const start = async () => {
