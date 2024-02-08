52b442b56d661544265f63c628cb4cca6bc05f6a
Leonardo Reyes Munoz <112450932+Leonardo-Reyes-Munoz@users.noreply.github.com>
2024-02-08 14:57:21 -0800

    feat(app.js) install and configure helmet, cors, xss-clean, and express-rate-limit npm packages.

--- app.js
+++ app.js
@@ -3,11 +3,26 @@ require('express-async-errors');
 
 const express = require('express');
 const app = express();
-
 // connectDB
 const connectDB = require('./db/connect');
 
+// security packages
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimiter = require('express-rate-limit');
+
+app.set('trust proxy', 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, //15 minutes
+    max: 100, //limit each IP to 100 request per windowsMs
+  })
+);
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 // Landing Page
 app.get('/', (req, res) => {
