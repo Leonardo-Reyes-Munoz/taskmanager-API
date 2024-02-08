0811d02bebbbe5d5e075921458fe5e93cb257a37
Leonardo Reyes Munoz <112450932+Leonardo-Reyes-Munoz@users.noreply.github.com>
2024-02-07 17:12:15 -0800

    feat(taskController.js): add createTask and getAllTasks logic.

--- controllers/taskControllers.js
+++ controllers/taskControllers.js
@@ -1,6 +1,17 @@
-const getAllTasks = async (req, res) => {};
+const Task = require('../models/Task');
 
-const createTask = async (req, res) => {};
+const getAllTasks = async (req, res) => {
+  const tasks = await Task.find({ createdBy: req.user.userId }).sort(
+    'CreatedAt'
+  );
+  res.status(200).json({ tasks, count: tasks.length });
+};
+
+const createTask = async (req, res) => {
+  req.body.createdBy = req.user.userId;
+  const task = await Task.create(req.body);
+  res.status(200).send(task);
+};
 
 const getTask = async (req, res) => {};
 
